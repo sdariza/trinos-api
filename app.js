501d87b89bda6bd3144d05456682a42399a5a693
sdariza <sdariza@uninorte.edu.co>
2021-09-27 22:18:19 -0500

    feat: Express initial setup + ESLint setup
    src folder created, public folder removed, eslint errors fixed.
    editorconfig added

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./src/routes/index');
+const usersRouter = require('./src/routes/users');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
